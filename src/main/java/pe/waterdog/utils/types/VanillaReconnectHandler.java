f3f05ab9a6e9825e1cfbf63b014fbc5b5f7614ca
TobiasDev <tobiasdevde@gmail.com>
2020-10-18 21:53:34 +0200

    New getFallbackServer Syntax

--- src/main/java/pe/waterdog/utils/types/VanillaReconnectHandler.java
+++ src/main/java/pe/waterdog/utils/types/VanillaReconnectHandler.java
@@ -2,12 +2,11 @@ package pe.waterdog.utils.types;
 
 import pe.waterdog.network.ServerInfo;
 import pe.waterdog.player.ProxiedPlayer;
-import pe.waterdog.utils.types.IReconnectHandler;
 
 public class VanillaReconnectHandler implements IReconnectHandler {
 
     @Override
-    public ServerInfo getFallbackServer(ProxiedPlayer p, ServerInfo oldServer) {
+    public ServerInfo getFallbackServer(ProxiedPlayer p, ServerInfo oldServer, String message) {
         return null;
     }
 }
