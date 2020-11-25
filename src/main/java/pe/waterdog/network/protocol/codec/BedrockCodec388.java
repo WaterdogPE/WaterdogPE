2432dcc1bf3980477422960a8da9be3fb72a0aed
Alemiz112 <alex@mizerak.eu>
2020-11-25 21:57:12 +0100

    Rewrite more packets

--- src/main/java/pe/waterdog/network/protocol/codec/BedrockCodec388.java
+++ src/main/java/pe/waterdog/network/protocol/codec/BedrockCodec388.java
@@ -1,17 +1,16 @@
-/**
+/*
  * Copyright 2020 WaterdogTEAM
- * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package pe.waterdog.network.protocol.codec;
@@ -71,6 +70,7 @@ public class BedrockCodec388 extends BedrockCodec {
                 .registerPacket(MobEffectPacket.class, MobEffectSerializer_v291.INSTANCE, 28)
                 .registerPacket(UpdateAttributesPacket.class, UpdateAttributesSerializer_v291.INSTANCE, 29)
                 .registerPacket(MobEquipmentPacket.class, MobEquipmentSerializer_v291.INSTANCE, 31)
+                .registerPacket(MobArmorEquipmentPacket.class, MobArmorEquipmentSerializer_v291.INSTANCE, 32)
                 .registerPacket(InteractPacket.class, InteractSerializer_v388.INSTANCE, 33)
                 .registerPacket(PlayerActionPacket.class, PlayerActionSerializer_v291.INSTANCE, 36)
                 .registerPacket(SetEntityDataPacket.class, SetEntityDataSerializer_v291.INSTANCE, 39)
--- src/main/java/pe/waterdog/network/rewrite/EntityMap.java
+++ src/main/java/pe/waterdog/network/rewrite/EntityMap.java
@@ -1,17 +1,16 @@
-/**
+/*
  * Copyright 2020 WaterdogTEAM
- * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package pe.waterdog.network.rewrite;
@@ -24,8 +23,7 @@ import pe.waterdog.network.rewrite.types.RewriteData;
 import pe.waterdog.player.PlayerRewriteUtils;
 import pe.waterdog.player.ProxiedPlayer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Class to map the proper entityIds to entity-related packets.
@@ -76,6 +74,12 @@ public class EntityMap implements BedrockPacketHandler {
     }
 
     @Override
+    public boolean handle(MobArmorEquipmentPacket packet) {
+        packet.setRuntimeEntityId(PlayerRewriteUtils.rewriteId(packet.getRuntimeEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
+        return true;
+    }
+
+    @Override
     public boolean handle(PlayerActionPacket packet) {
         packet.setRuntimeEntityId(PlayerRewriteUtils.rewriteId(packet.getRuntimeEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
         return true;
@@ -109,6 +113,16 @@ public class EntityMap implements BedrockPacketHandler {
     public boolean handle(AddPlayerPacket packet) {
         packet.setRuntimeEntityId(PlayerRewriteUtils.rewriteId(packet.getRuntimeEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
         packet.setUniqueEntityId(PlayerRewriteUtils.rewriteId(packet.getUniqueEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
+
+        ListIterator<EntityLinkData> iterator = packet.getEntityLinks().listIterator();
+        while (iterator.hasNext()){
+            EntityLinkData entityLink = iterator.next();
+            long from = PlayerRewriteUtils.rewriteId(entityLink.getFrom(), rewrite.getEntityId(), rewrite.getOriginalEntityId());
+            long to = PlayerRewriteUtils.rewriteId(entityLink.getTo(), rewrite.getEntityId(), rewrite.getOriginalEntityId());
+            if (entityLink.getFrom() != from || entityLink.getTo() != to){
+                iterator.set(new EntityLinkData(from, to, entityLink.getType(), entityLink.isImmediate(), entityLink.isRiderInitiated()));
+            }
+        }
         return true;
     }
 
@@ -116,6 +130,16 @@ public class EntityMap implements BedrockPacketHandler {
     public boolean handle(AddEntityPacket packet) {
         packet.setRuntimeEntityId(PlayerRewriteUtils.rewriteId(packet.getRuntimeEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
         packet.setUniqueEntityId(PlayerRewriteUtils.rewriteId(packet.getUniqueEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
+
+        ListIterator<EntityLinkData> iterator = packet.getEntityLinks().listIterator();
+        while (iterator.hasNext()){
+            EntityLinkData entityLink = iterator.next();
+            long from = PlayerRewriteUtils.rewriteId(entityLink.getFrom(), rewrite.getEntityId(), rewrite.getOriginalEntityId());
+            long to = PlayerRewriteUtils.rewriteId(entityLink.getTo(), rewrite.getEntityId(), rewrite.getOriginalEntityId());
+            if (entityLink.getFrom() != from || entityLink.getTo() != to){
+                iterator.set(new EntityLinkData(from, to, entityLink.getType(), entityLink.isImmediate(), entityLink.isRiderInitiated()));
+            }
+        }
         return true;
     }
 
@@ -190,16 +214,13 @@ public class EntityMap implements BedrockPacketHandler {
 
     @Override
     public boolean handle(PlayerListPacket packet) {
-        if (packet.getAction() != PlayerListPacket.Action.ADD) return false;
-
-        List<PlayerListPacket.Entry> entries = new ArrayList<>(packet.getEntries());
-        packet.getEntries().clear();
-
-        for (PlayerListPacket.Entry entry : entries) {
-            entry.setEntityId(PlayerRewriteUtils.rewriteId(entry.getEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
-            packet.getEntries().add(entry);
+        if (packet.getAction() != PlayerListPacket.Action.ADD){
+            return false;
         }
 
+        for (PlayerListPacket.Entry entry : packet.getEntries()) {
+            entry.setEntityId(PlayerRewriteUtils.rewriteId(entry.getEntityId(), rewrite.getEntityId(), rewrite.getOriginalEntityId()));
+        }
         return true;
     }
 
