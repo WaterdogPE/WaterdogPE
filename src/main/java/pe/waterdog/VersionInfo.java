51155c4b33126b9042fb6dc3f150893a068d0126
Alemiz112 <alex@mizerak.eu>
2020-12-25 22:15:29 +0100

    Update LATEST_PROTOCOL_VERSION to 422

--- src/main/java/pe/waterdog/VersionInfo.java
+++ src/main/java/pe/waterdog/VersionInfo.java
@@ -20,7 +20,7 @@ public interface VersionInfo {
 
     String BASE_VERSION = "1.0.0";
     String BUILD_VERSION = "#build";
-    int LATEST_PROTOCOL_VERSION = 419;
+    int LATEST_PROTOCOL_VERSION = 422;
     boolean IS_DEVELOPMENT = true;
     String AUTHOR = "WaterdogTEAM";
 }
