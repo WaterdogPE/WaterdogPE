3b6c0649db38a18039d0fa68a84bfdad553cb89d
Alemiz112 <alex@mizerak.eu>
2021-01-31 21:58:19 +0100

    Fix incorrect licence in copyright header

--- src/main/java/dev/waterdog/network/protocol/codec/BedrockCodec422.java
+++ src/main/java/dev/waterdog/network/protocol/codec/BedrockCodec422.java
@@ -1,10 +1,10 @@
 /*
  * Copyright 2021 WaterdogTEAM
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the GNU General Public License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
