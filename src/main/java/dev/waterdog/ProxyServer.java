cb75cdb4db03c16de053dd2e1a4603a5eb38ff6e
DerDevHD <59358798+DerDevHD@users.noreply.github.com>
2021-02-10 19:05:34 +0100

    Add args to DispatchCommandEvent (#46)

--- src/main/java/dev/waterdog/ProxyServer.java
+++ src/main/java/dev/waterdog/ProxyServer.java
@@ -221,14 +221,16 @@ public class ProxyServer {
     }
 
     public boolean dispatchCommand(CommandSender sender, String message) {
-        DispatchCommandEvent event = new DispatchCommandEvent(sender, message);
-        this.eventManager.callEvent(event);
+        String[] args = message.split(" ");
+        String[] shiftedArgs = Arrays.copyOfRange(args, 1, args.length);
+        DispatchCommandEvent event = new DispatchCommandEvent(sender, args[0], shiftedArgs);
 
+        this.eventManager.callEvent(event);
         if (event.isCancelled()) {
             return false;
         }
-        String[] args = message.split(" ");
-        return this.commandMap.handleCommand(sender, args[0], Arrays.copyOfRange(args, 1, args.length));
+
+        return this.commandMap.handleCommand(sender, args[0], shiftedArgs);
     }
 
     public CompletableFuture<BedrockClient> bindClient(ProtocolVersion protocol) {
