99ee8ccc39d625059700839d0b1210efb893f508
Vazzi <45903049+VazziDE@users.noreply.github.com>
2021-04-12 18:11:53 +0200

    Changed PluginsCommand and became compliant
    Removed spaces, Changed to easier way with String.join, added new private check method, added display plugin details(author, version) and how much plugins on the proxy are. Added Copyright Header.

--- src/main/java/dev/waterdog/command/defaults/PluginsCommand.java
+++ src/main/java/dev/waterdog/command/defaults/PluginsCommand.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 WaterdogTEAM
+ * Licensed under the GNU General Public License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.waterdog.command.defaults;
 
 import dev.waterdog.ProxyServer;
@@ -7,6 +22,8 @@ import dev.waterdog.command.CommandSettings;
 import dev.waterdog.plugin.Plugin;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class PluginsCommand extends Command {
 
@@ -18,18 +35,21 @@ public class PluginsCommand extends Command {
                 .build());
     }
 
-
     @Override
     public boolean onExecute(CommandSender sender, String alias, String[] args) {
-
-        ArrayList<String> PluginsListArr = new ArrayList<>();
-        for (Plugin p : ProxyServer.getInstance().getPluginManager().getPlugins()) {
-            PluginsListArr.add(p.getName());
+        List<String> pluginmap = new ArrayList<>();
+        Collection<Plugin> installedplugins = ProxyServer.getInstance().getPluginManager().getPlugins();
+        for (Plugin p : installedplugins) {
+            pluginmap.add(checkPlugin(p));
         }
-        String pluginsformat = PluginsListArr.toString().replace("[", "").replace("]", "");
-        sender.sendMessage("§aWaterdogPE Plugins: §e" + pluginsformat);
+        sender.sendMessage("§eWaterdogPE Plugins [" +  installedplugins.size() + "]: " + String.join("§7, ", pluginmap));
         return true;
     }
 
-
+    private String checkPlugin(Plugin targetplugin){
+        if(targetplugin.isEnabled()){
+            return "§a" + targetplugin.getName() + " : " + targetplugin.getDescription().getVersion() + " by " + targetplugin.getDescription().getAuthor();
+        }
+        return "§c" + targetplugin.getName() + " : " + targetplugin.getDescription().getVersion() + " by " + targetplugin.getDescription().getAuthor();
+    }
 }
