9cd97ed2f6dda25cafd387e49887729cbeae0a50
Alemiz112 <alex@mizerak.eu>
2021-06-11 21:17:42 +0200

    Next is v1.1.1

--- src/main/java/dev/waterdog/waterdogpe/VersionInfo.java
+++ src/main/java/dev/waterdog/waterdogpe/VersionInfo.java
@@ -21,7 +21,7 @@ public class VersionInfo {
 
     public static final boolean DEFAULT_DEBUG = true;
 
-    private final String baseVersion = "1.1.0";
+    private final String baseVersion = "1.1.1";
     private final String buildVersion = "#build";
     private final int latestProtocolVersion = 440;
     private final String author = "WaterdogTEAM";
