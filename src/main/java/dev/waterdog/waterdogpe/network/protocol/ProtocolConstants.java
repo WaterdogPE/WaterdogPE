ad53b3b42d8e0b8fd66a1ff311cbedbd865efffc
Alemiz <45739737+Alemiz112@users.noreply.github.com>
2022-02-12 16:10:54 +0100

    Add support for 1.18.10 (#154)

--- src/main/java/dev/waterdog/waterdogpe/network/protocol/ProtocolConstants.java
+++ src/main/java/dev/waterdog/waterdogpe/network/protocol/ProtocolConstants.java
@@ -78,6 +78,7 @@ public class ProtocolConstants {
         registerCodec(ProtocolVersion.MINECRAFT_PE_1_17_30, new BedrockCodec465());
         registerCodec(ProtocolVersion.MINECRAFT_PE_1_17_40, new BedrockCodec471());
         registerCodec(ProtocolVersion.MINECRAFT_PE_1_18_0, new BedrockCodec475());
+        registerCodec(ProtocolVersion.MINECRAFT_PE_1_18_10, new BedrockCodec486());
     }
 
     /**
