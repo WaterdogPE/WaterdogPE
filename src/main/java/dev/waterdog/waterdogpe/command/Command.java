bdd56b95d1ac36d2e420de0fbca0e817dca587b2
Metehan Gezer <irrelevantdev@outlook.com>
2022-06-09 17:08:44 +0300

    Fixed command itself is not visible when aliases present (#149) (#150)
    * Fixed command itself is not visible when aliases present
    
    * Use Set instead of List

--- src/main/java/dev/waterdog/waterdogpe/command/Command.java
+++ src/main/java/dev/waterdog/waterdogpe/command/Command.java
@@ -19,8 +19,9 @@ import com.nukkitx.protocol.bedrock.data.command.CommandData;
 import com.nukkitx.protocol.bedrock.data.command.CommandEnumData;
 import com.nukkitx.protocol.bedrock.data.command.CommandParam;
 import com.nukkitx.protocol.bedrock.data.command.CommandParamData;
+import org.apache.commons.lang3.ArrayUtils;
 
-import java.util.Collections;
+import java.util.*;
 
 /**
  * Base class for proxy commands
@@ -87,6 +88,9 @@ public abstract class Command {
         CommandParamData[][] parameterData = new CommandParamData[][]{{
                 new CommandParamData(this.name, true, null, CommandParam.TEXT, null, Collections.emptyList())
         }};
-        return new CommandData(this.name, this.getDescription(), Collections.emptyList(), (byte) 0, new CommandEnumData(this.name, getAliases(), false), parameterData);
+        Set<String> aliases = new HashSet<>(getAliases().length + 1);
+        Collections.addAll(aliases, getAliases());
+        aliases.add(this.name);
+        return new CommandData(this.name, this.getDescription(), Collections.emptyList(), (byte) 0, new CommandEnumData(this.name, aliases.toArray(ArrayUtils.EMPTY_STRING_ARRAY), false), parameterData);
     }
 }
