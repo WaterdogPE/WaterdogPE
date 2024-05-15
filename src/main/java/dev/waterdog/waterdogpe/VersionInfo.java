d98850dd78e4d481c5542d3e202454c07afaf18e
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2024-05-15 18:11:56 +0200

    Mark as 'official' build

--- src/main/java/dev/waterdog/waterdogpe/VersionInfo.java
+++ src/main/java/dev/waterdog/waterdogpe/VersionInfo.java
@@ -23,7 +23,7 @@ public class VersionInfo {
     public static final boolean DEFAULT_DEBUG = true;
 
     private final String baseVersion = "2.0.2";
-    private final String buildVersion = "#build";
+    private final String buildVersion = "syo";
     private final String author = "WaterdogTEAM, Syodo Development";
     private final int metricsId = 15678;
     private final int latestProtocolVersion;
