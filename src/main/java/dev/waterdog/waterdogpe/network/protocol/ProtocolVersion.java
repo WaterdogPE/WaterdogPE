d9bdff01caf183a0d09f6b690c788f696986e1dd
Alemiz <alex@mizerak.eu>
2024-06-14 19:24:05 +0200

    Add support for MCBE 1.21

--- src/main/java/dev/waterdog/waterdogpe/network/protocol/ProtocolVersion.java
+++ src/main/java/dev/waterdog/waterdogpe/network/protocol/ProtocolVersion.java
@@ -59,6 +59,7 @@ import org.cloudburstmc.protocol.bedrock.codec.v630.Bedrock_v630;
 import org.cloudburstmc.protocol.bedrock.codec.v649.Bedrock_v649;
 import org.cloudburstmc.protocol.bedrock.codec.v662.Bedrock_v662;
 import org.cloudburstmc.protocol.bedrock.codec.v671.Bedrock_v671;
+import org.cloudburstmc.protocol.bedrock.codec.v685.Bedrock_v685;
 
 @ToString(exclude = {"defaultCodec", "bedrockCodec"})
 public enum ProtocolVersion {
@@ -103,7 +104,8 @@ public enum ProtocolVersion {
     MINECRAFT_PE_1_20_50(630, Bedrock_v630.CODEC),
     MINECRAFT_PE_1_20_60(649, Bedrock_v649.CODEC),
     MINECRAFT_PE_1_20_70(662, Bedrock_v662.CODEC),
-    MINECRAFT_PE_1_20_80(671, Bedrock_v671.CODEC);
+    MINECRAFT_PE_1_20_80(671, Bedrock_v671.CODEC),
+    MINECRAFT_PE_1_21_0(685, Bedrock_v685.CODEC);
 
     private static final ProtocolVersion[] VALUES = values();
     private static final Int2ObjectMap<ProtocolVersion> VERSIONS = new Int2ObjectOpenHashMap<>();
