4d7a3323f08a0a88a2d422250f4c6d5f0ab4a617
Alemiz <alex@mizerak.eu>
2024-07-09 20:19:00 +0200

    Add support for MCBE 1.21.2

--- src/main/java/dev/waterdog/waterdogpe/network/protocol/ProtocolVersion.java
+++ src/main/java/dev/waterdog/waterdogpe/network/protocol/ProtocolVersion.java
@@ -105,7 +105,8 @@ public enum ProtocolVersion {
     MINECRAFT_PE_1_20_60(649, Bedrock_v649.CODEC),
     MINECRAFT_PE_1_20_70(662, Bedrock_v662.CODEC),
     MINECRAFT_PE_1_20_80(671, Bedrock_v671.CODEC),
-    MINECRAFT_PE_1_21_0(685, Bedrock_v685.CODEC);
+    MINECRAFT_PE_1_21_0(685, Bedrock_v685.CODEC),
+    MINECRAFT_PE_1_21_2(686, Bedrock_v685.CODEC.toBuilder().protocolVersion(686).build());
 
     private static final ProtocolVersion[] VALUES = values();
     private static final Int2ObjectMap<ProtocolVersion> VERSIONS = new Int2ObjectOpenHashMap<>();
