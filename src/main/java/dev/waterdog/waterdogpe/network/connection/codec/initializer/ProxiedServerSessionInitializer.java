60b605f6091edb0df2f672df73a3331d8dbdb9d0
irrld <irrelevantdev@outlook.com>
2026-08-11 15:33:45 +0300

    Disable auto flush

--- src/main/java/dev/waterdog/waterdogpe/network/connection/codec/initializer/ProxiedServerSessionInitializer.java
+++ src/main/java/dev/waterdog/waterdogpe/network/connection/codec/initializer/ProxiedServerSessionInitializer.java
@@ -53,6 +53,7 @@ public class ProxiedServerSessionInitializer extends ProxiedSessionInitializer<B
         if (metrics instanceof RakChannelMetrics rakMetrics) {
             channel.config().setOption(RakChannelOption.RAK_METRICS, rakMetrics);
         }
+        channel.config().setOption(RakChannelOption.RAK_AUTO_FLUSH, false);
 
         super.initChannel(channel);
 
